f158070e41b938dcb0a0d19a67df9679ac524983
Tobias Wiedenhöfer <twiedenhoefer@dbkg.de>
2018-04-06 14:48:40 +0200

    no message

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,10 @@
 
 namespace tw88\sso;
 
-return function () {
-    echo "hi!";
+use Illuminate\Contracts\Bus\Dispatcher as Bus;
+use Illuminate\Contracts\Events\Dispatcher;
+use tw88\sso\Listener;
+
+return function (Dispatcher $events, Bus $bus) {
+    $events->subscribe(Listener\AddConfigureMiddleware::class);
 };
